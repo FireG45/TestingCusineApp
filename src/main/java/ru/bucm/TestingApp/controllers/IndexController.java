fb1716a37ffd0157dc943cd610c3f2669529585d
FireG45 <cace.200218@gmail.com>
2023-11-27 00:27:56 +0300

    first commit

---
+++ src/main/java/ru/bucm/TestingApp/controllers/IndexController.java
@@ -0,0 +1,10 @@
+package ru.bucm.TestingApp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    public void index() {}
+}
